72a95cf9be6c9a78681e1cabc2a6a7a67b33d48d
orlahyhemmie <olaniranolayemi16@gmail.com>
2024-12-18 10:46:58 +0000

    first commit

---
+++ fronten/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // eslint-disable-next-line no-empty-pattern
+  const [] = useState(0)
+
+  return (
+    <div>
+      <header>
+        TS Amazona
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        All right reserved
+      </footer>
+    </div>
+     
+  )
+}
+
+export default App
